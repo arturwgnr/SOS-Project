9caeb570aa22bbe749bbf6ffd8effdc53b703a65
Artur Wagner <arturwagner99@hotmail.com>
2025-10-29 09:56:20 +0000

    Backend signature fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const localHost = `http://localhost:5000/`;
 
